82ddb9f5a18be7405acdacb168306786c50ee578
Greg Caporaso <gregcaporaso@gmail.com>
2010-02-05 18:09:36 +0000

    Updated to check PyCogent version.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # File created on 04 Feb 2010
 from __future__ import division
 from distutils.core import setup
+import re
 
 __author__ = "Greg Caporaso"
 __copyright__ = "Copyright 2010, The PyNAST project"
@@ -20,7 +21,19 @@ J. Gregory Caporaso, Kyle Bittinger, Frederic D. Bushman, Todd Z. DeSantis, Gary
 January 15, 2010, DOI 10.1093/bioinformatics/btp636. Bioinformatics 26: 266-267.
 
 """
- 
+try:
+    import cogent
+except ImportError:
+    print "PyCogent not installed but required. (Is it installed? Is it in the current users $PYTHONPATH or site-packages?) See http://pycogent.sourceforge.net."
+    exit(1)
+    
+pycogent_version = tuple([int(v) \
+        for v in re.split("[^\d]", cogent.__version__) if v.isdigit()])
+        
+if pycogent_version < (1,4,0):
+    print "PyCogent >= 1.4.0 required, but %s is installed." % cogent.__version__
+    exit(1)
+    
 setup(name='PyNAST',
       version=__version__,
       description='The Python Nearest Alignment Search Tool',
