fa70a3994c2513ac4063396110aed5733e4b5952
Greg Caporaso <gregcaporaso@gmail.com>
2010-04-05 17:58:32 +0000

    Added descriptive code to __init__.py.

---
+++ pynast/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = "The PyNAST Development Team"
+__copyright__ = "Copyright 2010, The QIIME Project"
+__credits__ = ["Greg Caporaso", "Kyle Bittinger", "Rob Knight"] 
+__license__ = "GPL"
+__version__ = "1.1-dev"
+__maintainer__ = "Greg Caporaso"
+__email__ = "gregcaporaso@gmail.com"
+__status__ = "Pre-release"
+
+__all__ = ['logger','util']
