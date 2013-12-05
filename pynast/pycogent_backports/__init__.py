a767bd90d6901fd33e6d9db1040f317beff4defa
Greg Caporaso <gregcaporaso@gmail.com>
2013-12-04 21:24:16 -0700

    added license info to __init__

---
+++ pynast/pycogent_backports/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, The PyNAST Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+__all__ = ['uclust']
